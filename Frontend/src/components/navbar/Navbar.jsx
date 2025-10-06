9e89d1ae717caae2786c2009f1856dffc3e1d313
Prema Ghosh <premaghosh31@gmail.com>
2025-10-06 20:06:12 +0530

    2nd commit

---
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,68 @@
+import { useContext, useState } from "react";
+import "./navbar.scss";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const { currentUser } = useContext(AuthContext);
+  const number = 4;
+
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="" />
+          <span>RoomConnect</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        {currentUser ? (
+          <div className="user">
+            <img src={currentUser.avatar || "/noavatar.jpg"} alt="" />
+            <span>{currentUser.username}</span>
+            <Link to="/profile" className="profile">
+              {number > 0 && <div className="notification">{number}</div>}
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <a href="/login">Sign in</a>
+            <a href="/register" className="register">
+              Sign up
+            </a>
+            {/* <Link to="/login" >
+              <span>Sign in</span>
+            </Link>
+            <Link to="/register" className="register">
+              <span>Sign up</span>
+            </Link> */}
+          </>
+        )}
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/login">Sign in</a>
+          <a href="/signup">Sign up</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
